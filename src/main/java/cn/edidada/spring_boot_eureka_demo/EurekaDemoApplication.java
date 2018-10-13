018dfb237a6cad10e7acbf8a0b5a210db3b5aa59
edidada <1664884095@qq.com>
2018-10-13 22:00:45 +0800

    test commit

---
+++ src/main/java/cn/edidada/spring_boot_eureka_demo/EurekaDemoApplication.java
@@ -0,0 +1,14 @@
+package cn.edidada.spring_boot_eureka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDemoApplication.class, args);
+    }
+}
